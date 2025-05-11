3707e65194666cd02716bfd6c441e07490c947bf
Vasily Romanov <romanovve@yandex.ru>
2025-05-11 23:00:36 +0300

    добавил тесты проекта 4 спринта

---
+++ src/main/java/Constant/DataForOrder.java
@@ -0,0 +1,20 @@
+package Constant;
+
+public class DataForOrder {
+    public static final String NAME = "Александр";
+    public static final String LAST_NAME = "Пушкин";
+    public static final String ADDRESS = "г. Москва, ул. Цветной Бульвар д.2";
+    public static final String METRO = "Тургеневская";
+    public static final String PHONE = "899988887766";
+
+    public static final String DATE = "10.05.2025";
+    public static final String COMMENT = "Лучше не звонить!";
+
+    public static final String BLACK = "black";
+    public static final String GREY = "grey";
+
+    public static final String ONE_DAY = "сутки";
+    public static final String SEVEN_DAYS = "семеро суток";
+
+    public static final String INVALID_ORDER_NUMBER = "770";
+}
